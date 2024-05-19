ed4e8f90296a21fe2403080a9d72169e51ffe6cc
nvnhat95 <nguyenvietnhat95@gmail.com>
2024-05-19 13:23:33 +0700

    test 2.5dgs without loss changes

--- model/splatting_avatar_model.py
+++ model/splatting_avatar_model.py
@@ -182,6 +182,8 @@ class SplattingAvatarModel(GaussianBase):
 
         dist2 = torch.clamp_min(distCUDA2(torch.from_numpy(np.asarray(pcd.points)).float().to(self.device)), 0.0000001)
         scales = torch.log(torch.sqrt(dist2))[...,None].repeat(1, 3)
+        # nhatdm
+        scales[:, 2] = torch.log(torch.tensor(0))
         rots = torch.zeros((fused_point_cloud.shape[0], 4), device="cuda")
         rots[:, 0] = 1
 
